d0ecc507190fca716118749fe960e8091998237b
lazin87 <rafal.lazinski@gmail.com>
2015-08-26 11:58:01 +0200

    php dev

---
+++ php/newRow.php
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<response>
+    <sessionId><![CDATA[123456SessionId]]></sessionId>
+    <message>
+        <value><![CDATA[Login successful]]></value>
+    </message>
+</response>
